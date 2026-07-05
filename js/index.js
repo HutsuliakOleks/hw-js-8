1efef47f7db22b972e6ee784826c82e8ff0ac10a
HutsuliakOleks <sashaguts05@gmail.com>
2026-07-05 20:18:00 +0300

    Я доробив вправи в методі масиву JS (ДЗ8)

--- js/index.js
+++ js/index.js
@@ -2,8 +2,5 @@ const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
 
 let string = "";
 
-for (string of friends) {
-  console.log(string);
-}
 let textString = friends.join(", ");
 console.log(textString);
